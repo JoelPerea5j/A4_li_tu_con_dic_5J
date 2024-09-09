2260bfde04f24ad07347497f752dea9c43971ea3
JoelPerea5j <a.22308051281272@cbtis128.edu.mx>
2024-09-09 10:12:59 -0600

    Add files via upload

---
+++ Mi_lista.py
@@ -0,0 +1,17 @@
+print("Ejemplo de listas")
+arcoiris=["Verde","Azul", "Morado"]
+print(arcoiris)
+longitud=len(arcoiris)
+print("Elementos que contiene la lista arcoiris es de:", longitud)
+print(f"Elementos que contiene la lista arcoiris v2 es de:  {longitud}")
+print("Accediendo a un elemento de mi lista")
+print(arcoiris[1])
+print(f"Elemento en la posicion 0 es {arcoiris[0]}")
+print("Agregar un elemento de mi lista")
+print(arcoiris)
+arcoiris.append("Naranja")
+print(arcoiris)
+print("Listar los elementos de la lista ciclo for")
+for Perea in arcoiris:
+    print(Perea)
+
---
+++ mis_tuplas.py
@@ -0,0 +1,13 @@
+print("Ejemplo de tuplas")
+canciones=("Te quiero","El NoNoa","Amor Eterno")
+print(canciones)
+y = list(canciones)
+print(y)
+y[1] = "La puerta negra"
+x = tuple(y)
+print(x)
+
+
+
+
+
