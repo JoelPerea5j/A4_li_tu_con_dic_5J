ff4151ec701fe68af27202ee9af67022ba94df68
JoelPerea5j <a.22308051281272@cbtis128.edu.mx>
2024-09-11 07:37:08 -0600

    Add files via upload
    si

---
+++ mi_conjunto.py
@@ -0,0 +1,18 @@
+print("Ejemplo de conjuntos set")
+tipodeportes={"Futbol", "Tenis", "Basquetbol"}
+print(tipodeportes)
+print("Lista de elemento del conjunto tipodeportes loop for")
+print(tipodeportes)
+for Perea in tipodeportes:
+    print(Perea)
+
+
+
+
+
+
+
+
+
+
+
